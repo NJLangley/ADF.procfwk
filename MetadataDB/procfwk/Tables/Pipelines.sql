70a088c578c8654be11202fa726e43ca24a00fc4
Niall Langley <niall.langley@gmail.com>
2021-02-24 18:03:16 +0000

    Added unique index top pipelines LogicalUsageValue column

--- MetadataDB/procfwk/Tables/Pipelines.sql
+++ MetadataDB/procfwk/Tables/Pipelines.sql
@@ -1,16 +1,19 @@
 ﻿CREATE TABLE [procfwk].[Pipelines] (
-    [PipelineId]   INT            IDENTITY (1, 1) NOT NULL,
-    [OrchestratorId] INT NOT NULL,
-    [StageId]      INT            NOT NULL,
-    [PipelineName] NVARCHAR (200) NOT NULL,
-    [LogicalPredecessorId] INT NULL,
-    [Enabled]      BIT CONSTRAINT [DF_Pipelines_Enabled] DEFAULT ((1)) NOT NULL,
+    [PipelineId]           INT            IDENTITY (1, 1) NOT NULL,
+    [OrchestratorId]       INT            NOT NULL,
+    [StageId]              INT            NOT NULL,
+    [PipelineName]         NVARCHAR (200) NOT NULL,
+    [LogicalPredecessorId] INT            NULL,
+    [Enabled]              BIT            CONSTRAINT [DF_Pipelines_Enabled] DEFAULT ((1)) NOT NULL,
     [LogicalUsageValue]    VARCHAR (255)  NOT NULL,
     CONSTRAINT [PK_Pipelines] PRIMARY KEY CLUSTERED ([PipelineId] ASC),
+    CONSTRAINT [FK_Pipelines_Orchestrators] FOREIGN KEY ([OrchestratorId]) REFERENCES [procfwk].[Orchestrators] ([OrchestratorId]),
+    CONSTRAINT [FK_Pipelines_Pipelines] FOREIGN KEY ([LogicalPredecessorId]) REFERENCES [procfwk].[Pipelines] ([PipelineId]),
     CONSTRAINT [FK_Pipelines_Stages] FOREIGN KEY ([StageId]) REFERENCES [procfwk].[Stages] ([StageId]),
-    CONSTRAINT [FK_Pipelines_Orchestrators] FOREIGN KEY([OrchestratorId]) REFERENCES [procfwk].[Orchestrators] ([OrchestratorId]),
-    CONSTRAINT [FK_Pipelines_Pipelines] FOREIGN KEY([LogicalPredecessorId]) REFERENCES [procfwk].[Pipelines] ([PipelineId])
+    CONSTRAINT [UQ_Pipelines_#LogicalUsageValue] UNIQUE NONCLUSTERED ([LogicalUsageValue] ASC)
 );
+
+
 
 
 
